51f15febdd52abda45338386836ac857fc9c5d47
869358189@qq.com <869358189@qq.com>
2020-10-27 10:18:27 +0800

    AC

---
+++ src/main/java/com/tianyongwei/playground/class_level/package1/Package1Class2.java
@@ -0,0 +1,8 @@
+package com.tianyongwei.playground.class_level.package1;
+
+public class Package1Class2 {
+
+    public static void main(String[] args) {
+//        new Package1Class1.Package1ProtectedClass1();
+    }
+}
---
+++ src/test/java/com/tianyongwei/playground/closure/ClosureTestPackage1Class1.java
@@ -0,0 +1,12 @@
+package com.tianyongwei.playground.closure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClosureTestPackage1Class1 {
+
+    @Test
+    void main() {
+    }
+}
